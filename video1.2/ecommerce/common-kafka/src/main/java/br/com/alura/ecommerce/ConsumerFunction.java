df454bff320fe72a07526958a838a431b2071c1c
Caio César Mendonça Souza <caiocesar.msouza@gmail.com>
2019-12-17 13:48:58 -0300

    aula2

--- video1.2/ecommerce/common-kafka/src/main/java/br/com/alura/ecommerce/ConsumerFunction.java
+++
@@ -1,9 +0,0 @@
-package br.com.alura.ecommerce;
-
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-
-import java.util.concurrent.ExecutionException;
-
-public interface ConsumerFunction<T> {
-    void consume(ConsumerRecord<String, T> record) throws ExecutionException, InterruptedException;
-}
